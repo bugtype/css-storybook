7032476c55fe5ecb9ed11670470e88742be5e405
bugtype <bugtype.kr@gmail.com>
2021-04-02 14:31:45 +0900

    feature(tabs): tabs 하나 추가

---
+++ src/components/TypeAnimation/TypeAnimation.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './typing-animation.css';
+
+export const TypeAnimation = ({ text } : any) => (
+  <div className="text-typing">
+    <p>{text}</p>
+  </div>
+);
