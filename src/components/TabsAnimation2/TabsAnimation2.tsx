5e63e0b1ffba34b65f902341a3407f7bd542f8be
bugtype <bugtype.kr@gmail.com>
2021-04-06 11:31:26 +0900

    fix: 프로젝트 셋팅 수정
    add(tabs): tabs animation 추가

---
+++ src/components/TabsAnimation2/TabsAnimation2.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+import "./tabs-animation2.css";
+
+export const TabsAnimation2 = () => {
+  const openCity = (cityName: string) => (evt: any) => {
+    const tabcontent = document.getElementsByClassName("tab2content");
+    for (let i = 0; i < tabcontent.length; i++) {
+      const tab = tabcontent[i];
+      tab.className = tab.className.replace("tab2content-active", "");
+    }
+    const tablinks = document.getElementsByClassName("tab2links");
+    for (let i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    const el = document.getElementById(cityName);
+    el!.classList.add("tab2content-active");
+    evt.currentTarget.className += " active";
+  };
+  return (
+    <>
+      <div className="tab2">
+        <button className="tab2links" onClick={openCity("London")}>
+          London
+        </button>
+        <button className="tab2links" onClick={openCity("Paris")}>
+          Paris
+        </button>
+        <button className="tab2links" onClick={openCity("Tokyo")}>
+          Tokyo
+        </button>
+      </div>
+      <div className="tab2-contnent-container">
+      <div id="London" className="tab2content tab2content-active">
+        <h3>London</h3>
+        <p>London is the capital city of England.</p>
+      </div>
+
+      <div id="Paris" className="tab2content">
+        <h3>Paris</h3>
+        <p>Paris is the capital of France.</p>
+      </div>
+
+      <div id="Tokyo" className="tab2content">
+        <h3>Tokyo</h3>
+        <p>Tokyo is the capital of Japan.</p>
+      </div>
+      </div>
+    </>
+  );
+};
--- src/stories/TabsAnimation.stories.tsx
+++ src/stories/TabsAnimation.stories.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { TabsAnimation } from "../components/TabsAnimation";
+import { TabsAnimation2 } from "../components/TabsAnimation2";
 
 export default {
   title: "React/Tabs",
@@ -9,7 +10,15 @@ export default {
 
 const Template = (args: any) => <TabsAnimation {...args} />;
 
+const Template2 = (args: any) => <TabsAnimation2 {...args} />;
+
 export const Tabs1 = Template.bind({});
 Tabs1.args = {
+  text: "Welcome bugtype",
+};
+
+
+export const Tabs2 = Template2.bind({});
+Tabs2.args = {
   text: "Welcome bugtype",
 };
