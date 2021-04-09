067f855122e758fccb002eae0bc185f0cff961b4
bugtype <bugtype.kr@gmail.com>
2021-04-09 10:48:27 +0900

    feature(loading): loading animation 추가

---
+++ src/components/LoadingAnimation/LoadingAnimation.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './loading-animation.css';
+
+export const LoadingAnimation = () => (
+    <div className="loadingBox">
+        <div className="dim"></div>
+        <div className="circle"></div>
+    </div>
+);
