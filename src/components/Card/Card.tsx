a2ec2efbb4940a3012eb5f2b18974d744cb67c78
bugtype <bugtype.kr@gmail.com>
2021-04-15 11:57:08 +0900

    feature: card css 추가

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './card.css';
+
+export const Card = () => (
+    <div className="background">
+        <div className="card">
+            <div className="header">
+            Card 1
+            </div>
+        </div>
+        <div className="card">
+            <div className="header">
+            Card 1
+            </div>
+        </div>
+        <div className="card">
+            <div className="header">
+            Card 1
+            </div>
+        </div>
+    </div>
+);
