b6d437483d55da3e2cd1912554fd0bea9619f439
bugtype <bugtype.kr@gmail.com>
2021-04-19 11:45:08 +0900

    temp

---
+++ src/components/LoadingAnimation3/LoadingAnimation3.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './loading-animation.css';
+
+export const LoadingAnimation3 = () => {
+
+
+    return (
+        <div >
+            <section>
+                <div className="loader3" />
+            </section>
+        </div>
+    );
+    
+}
--- src/stories/LoadingAnimation.stories.tsx
+++ src/stories/LoadingAnimation.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { LoadingAnimation } from '../components/LoadingAnimation';
 import { LoadingAnimation2 } from '../components/LoadingAnimation2';
+import { LoadingAnimation3 } from '../components/LoadingAnimation3';
 
 export default {
   title: 'React/LoadingAnimation',
@@ -10,6 +11,7 @@ export default {
 
 const Template = (args: any) => <LoadingAnimation {...args} />;
 const LoadingAnimation2Template = (args: any) => <LoadingAnimation2 {...args} />;
+const LoadingAnimation3Template = (args: any) => <LoadingAnimation3 {...args} />;
 
 export const LoadingAnimation_1 = Template.bind({});
 LoadingAnimation_1.args = {
@@ -21,3 +23,10 @@ export const LoadingAnimation_2 = LoadingAnimation2Template.bind({});
 LoadingAnimation_2.args = {
   text: 'Welcome bugtype',
 };
+
+export const LoadingAnimation_3 = LoadingAnimation3Template.bind({});
+LoadingAnimation_3.args = {
+  url: 'https://webdesign.tutsplus.com/ko/tutorials/a-beginners-introduction-to-css-animation--cms-21068',
+  custom_url: 'https://matthewlein.com/tools/ceaser',
+};
+
