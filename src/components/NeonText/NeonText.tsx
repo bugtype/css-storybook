a6f60e18800a448ae2bd25b943837cad00df724d
bugtype <bugtype.kr@gmail.com>
2021-04-23 14:48:19 +0900

    feat(text): neon text animamion

---
+++ src/components/NeonText/NeonText.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './neon-text.css';
+
+export const NeonText = () => (
+    <div className="neon-text-container">
+        <div className="neon-wrapper">
+            <span className="txt">hello</span>
+            <span className="gradient" />
+            <span className="dodge" />
+        </div>
+    </div>
+);
